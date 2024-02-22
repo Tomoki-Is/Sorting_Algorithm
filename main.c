cd9e80be6139f7cd555ad6076dadb6509caf66cc
Tomoki Ishizuka <tomoking5000@gmail.com>
2024-02-22 22:02:49 +0900

    Refactoring

--- main.c
+++ main.c
@@ -9,58 +9,78 @@
 #include"Selection_sort.c"
 #include"Shell_sort.c"
 
-int select = 6;
+typedef void (*Func)(int list[], int n);
+Func Sort;
+
+int Input(void){
+    printf("Input number of sorting\n");
+    printf("0 : Bubble sort\n");
+    printf("1 :  sort\n");
+    printf("2 : Insertion sort\n");
+    printf("3 : Selection sort\n");
+    printf("4 :  sort\n");
+    printf("5 : Selection sort\n");
+    printf("6 : Selection sort(recursive ver.)\n");
+    printf("7 : Shell sort\n");
+    printf("Input :");
+    int number;
+    scanf("%d", &number);
+    return number;
+}
+
+void Select_Sort(int select){
+    if(select == 0){
+      Sort=&Bubble_sort;
+    }
+    // else if(select == 1){
+    //   Sort=&Heap_sort;
+    // }
+    else if(select == 2){
+      Sort=&Insertion_sort;
+    }
+    // else if(select == 3){
+    //   Sort=&Merge_sort;
+    // }
+    // else if(select == 4){
+    //   Sort=&Quick_sort;
+    // }
+    else if(select == 5){
+      Sort=&Selection_sort;
+    }
+    else if(select == 6){
+      Sort=&RecursiveSelection_sort;
+    }
+    else if(select == 7){
+      Sort=&Shell_sort;
+    }
+}
 
 int main(void)
 {
-  typedef void (*Func)(int list[], int n);
-  Func Sort;
-  
-  if(select == 0){
-    Sort=&Bubble_sort;
-  }
-  // else if(select == 1){
-  //   Sort=&Heap_sort;
-  // }
-  else if(select == 2){
-    Sort=&Insertion_sort;
-  }
-  // else if(select == 3){
-  //   Sort=&Merge_sort;
-  // }
-  // else if(select == 4){
-  //   Sort=&Quick_sort;
-  // }
-  else if(select == 5){
-    Sort=&Selection_sort;
-  }
-  else if(select == 6){
-    Sort=&RecursiveSelection_sort;
-  }
-  else if(select == 7){
-    Sort=&Shell_sort;
-  }
+    int select = Input();
+    Select_Sort(select);
 
-  clock_t start, end;
-  start = clock();
+    clock_t start, end;
+    start = clock();
 
-  long long int loop = 100000000; // ループ回数
+    long long int loop = 100000000; // ループ回数
 
-  for(int i = 0; i < loop; i++){
-    int A[] = {4, 6, 7, 2, 1, 8, 9, 3, 5}; // 並び変える配列
-    int n = sizeof(A) / sizeof(A[0]); // 配列長の取得
-    
-    Sort(A, n);
+    for(int i = 0; i < loop; i++){
 
-    // for(int j = 0; j < n; j++){
-    //   printf("%d ", A[j]);
-    // }
-    // printf("\n");
-  }
+      int A[] = {4, 6, 7, 2, 1, 8, 9, 3, 5}; // 並び変える配列
+      int n = sizeof(A) / sizeof(A[0]); // 配列長の取得
+      
+      Sort(A, n);
 
-  end = clock();
-  double used_time = (end - start) / (double)loop;
-  printf("Calculation time is %f seconds.\n", used_time);
+      // for(int j = 0; j < n; j++){
+      //   printf("%d ", A[j]);
+      // }
+      // printf("\n");
+    }
 
-  return 0;
+    end = clock();
+    double used_time = (end - start) / (double)loop;
+    printf("Calculation time is %f seconds.\n", used_time);
+
+    return 0;
 }
