251050c15f3df1127101a3f5e46a4f28cfd020ee
Tomoki Ishizuka <tomoking5000@gmail.com>
2024-11-01 03:45:00 +0900

    fix:Insertion_sort

--- Insertion_sort.c
+++ Insertion_sort.c
@@ -2,7 +2,7 @@ void Insertion_sort(int a[], int n){
     for (int i = 1; i < n; i++){
         int j = i;
         int x = a[i];
-        while (a[j - 1] > x){
+        while (j > 0 && a[j - 1] > x){
             a[j] = a[j - 1];
             j--;
         }
